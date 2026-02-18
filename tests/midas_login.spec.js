79de3cffc426244012bf5d1c8515ac3ed0e9122b
rutus <rutus@phoenix.tech>
2026-02-18 15:01:44 -0600

    added traceroute in single file

--- tests/midas_login.spec.js
+++ tests/midas_login.spec.js
@@ -1,6 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Midas login text', async ({ page, context }) => {
+
+  await context.tracing.start(
+    { 
+      snapshots: true, 
+      screenshots: true 
+    });
   await page.goto('https://gsops-midas-admin-new.pi108.net:9012/login');
   await page.getByRole('textbox', { name: 'Username' }).click();
   await page.getByRole('textbox', { name: 'Username' }).fill('Rutus');
@@ -15,4 +21,6 @@ test('test', async ({ page }) => {
   await page.locator('div:nth-child(3) > .home-management').click();
   const page1 = await page1Promise;
   await page1.locator('.home-management').first().click();
+
+  await context.tracing.stop({path:'login_trace.zip'})
 });
