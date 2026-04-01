785c2677e6024209efe107ad873a530e335f0876
rutus-code <rutushah105@gmail.com>
2026-03-31 21:28:49 -0500

    added api authentication

--- tests/usePageObject.spec.ts
+++ tests/usePageObject.spec.ts
@@ -1,6 +1,6 @@
 import {test, expect} from '@playwright/test';
 import {PageManager} from '../page-objects/pageManager.ts';
-
+import {faker} from '@faker-js/faker';
 
 test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200/');
